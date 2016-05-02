049c34d619157249c1ec3155507042b8659126dd
Vasily Kirichenko <vaskir@gmail.com>
2016-05-02 11:18:05 +0300

    init

---
+++ Script.fsx
@@ -0,0 +1,94 @@
+﻿#r @"akka\Akka\System.Collections.Immutable.dll"
+#r @"packages\Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
+#r @"packages\FsPickler\lib\net45\FsPickler.dll"
+#r @"akka\Akka.Streams\System.Reactive.Streams.dll"
+#r @"akka\Akka.Streams\Akka.dll"
+#r @"akka\Akka.Streams\Akka.Streams.dll"
+#r @"akka\Akka.FSharp\Akka.FSharp.dll"
+
+open Akka.FSharp
+open Akka.Streams
+open Akka.Streams.Dsl
+open System.Diagnostics
+open System.Net
+open System
+open Microsoft.FSharp.Core.Printf
+open System.Threading.Tasks
+
+type RUnit = System.Reactive.Streams.Unit
+
+let rec fib n = if n < 2 then n else fib (n - 1) + fib (n - 2)
+let tc f =
+    let sw = Stopwatch.StartNew()
+    f() |> ignore
+    sw.Stop()
+    printfn "Elapsed %O" sw.Elapsed
+
+module Source =
+    let inline ofSeq x = Source.From x
+    let inline buffer size strategy (src: Source<_,_>) = src.Buffer(size, strategy)
+    let inline map (mapper: 'a -> 'b) (src: Source<_,_>) = src.Map(fun x -> mapper x)
+
+    let inline mapAsync parallelism (mapper: 'a -> Async<'b>) (src: Source<_,_>) = 
+        src.MapAsync(parallelism, fun x -> Async.StartAsTask (mapper x))
+    
+    let inline runForeach materializer (action: 'a -> unit) (src: Source<_,_>) =
+        src.RunForeach(System.Action<_> action, materializer) |> Async.AwaitTask
+
+    let inline named name (src: Source<_,_>) = src.Named name
+
+[<AutoOpen>]
+module Utils =
+    let (=>) (src: Source<_,_>) flow = src.Via(flow)
+    let (=>|) (src: Source<_,_>) sink = src.To(sink)
+    let print fmt = kprintf (printfn "%O %s" DateTime.Now) fmt
+
+let system = System.create "test" (Configuration.defaultConfig())
+let materializer = ActorMaterializer.Create system
+
+let source =
+    ["google.com"; "ya.ru"; "microsoft.com"; "jet.com"; "ycombinator.com"]
+    |> Source.ofSeq
+    |> Source.map (fun x -> Uri ("http://" + x))
+   
+let download (uri: Uri) : Task<Uri * string> =
+    async {
+        use client = new WebClient()
+        let! content = client.AsyncDownloadString uri
+        return uri, content
+    } |> Async.StartAsTask
+
+let graph = 
+    source
+        => Flow.Create<_>().MapAsyncUnordered (10, fun x -> download x)
+        => Flow.Create<_>().Map(fun (uri, content) -> print "%O, %d" uri content.Length; uri)
+        =>| Sink.Ignore()
+
+//source.RunForeach((fun uri -> print "%O" uri), materializer)
+
+graph.Run(materializer)
+
+//["google.com"; "ya.ru"; "microsoft.com"; "jet.com"; "ycombinator.com"]
+//|> Source.ofSeq
+//|> Source.map (fun x -> Uri ("http://" + x))
+//|> Source.mapAsync 10 (fun url -> 
+//    async {
+//        use client = new WebClient()
+//        let! content = client.AsyncDownloadString url
+//        return url, content
+//    })
+//|> Source.map (fun x -> printfn "%O %O downloaded" DateTime.Now (fst x); x)
+//|> Source.buffer 3 OverflowStrategy.Backpressure
+//|> Source.runForeach materializer (fun (url, content) ->
+//    printfn "%O %O => %d bytes" DateTime.Now url content.Length
+//    Thread.Sleep 5000)
+//|> Async.RunSynchronously
+
+//tc (fun _ -> 
+//    [0..42]
+//    |> Source.ofSeq
+//    |> Source.buffer 10 OverflowStrategy.Backpressure
+//    |> Source.mapAsync 4 (fun x -> async { return x, fib x })
+//    |> Source.runForeach materializer (fun (x, fib) -> printfn "fib %d = %d" x fib)
+//    |> Async.RunSynchronously
+//    )
---
+++ WorkDistribution.fsx
@@ -0,0 +1,67 @@
+﻿#r @"akka\Akka\System.Collections.Immutable.dll"
+#r @"packages\Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
+#r @"packages\FsPickler\lib\net45\FsPickler.dll"
+#r @"akka\Akka.Streams\System.Reactive.Streams.dll"
+#r @"akka\Akka.Streams\Akka.dll"
+#r @"akka\Akka.Streams\Akka.Streams.dll"
+#r @"akka\Akka.FSharp\Akka.FSharp.dll"
+
+open Akka.FSharp
+open Akka.Streams
+open Akka.Streams.Dsl
+open System
+open Microsoft.FSharp.Core.Printf
+open System.Threading
+
+type RUnit = System.Reactive.Streams.Unit
+
+[<AutoOpen>]
+module Utils =
+    let (=>) (src: Source<_,_>) flow = src.Via(flow)
+    let (=>|) (src: Source<_,_>) sink = src.To(sink)
+    let print fmt = kprintf (printf "%O %s" DateTime.Now) fmt
+
+let callService url input =
+    async {
+        Console.WriteLine (print "Calling %s with %s..." url input)
+        do! Async.Sleep 5000
+        return input
+    }
+
+let process' machine input =
+    let url = sprintf "http://%s/webservice/endpoint" machine
+    async {
+        return! callService url input
+    }
+
+let distribute (workers: ('a -> Async<'b>)[]) =
+    GraphDsl.Create(fun b ->
+        let balancer = b.Add(Balance<_>(workers.Length))
+        let merger = b.Add(Merge<_>(workers.Length))
+        
+        workers |> Array.iteri (fun i worker ->
+            b.From(balancer.Out i).Via(Flow.Create<_>().MapAsync(1, fun x -> Async.StartAsTask(worker x))).To(merger.In i) |> ignore)
+        
+        FlowShape(balancer.In, merger.Out)
+    )
+
+//let parallel' (workerCount: int) (flow: Flow<'a, 'b, RUnit>) =
+//    Array.init workerCount (fun _ -> flow) |> distribute
+
+let system = System.create "test" (Configuration.defaultConfig())
+let materializer = ActorMaterializer.Create system
+
+let renditions (machines: string list) (inputs: string[]) =
+    let workerCount = 4
+
+    let flows =
+        [| for machine in machines do
+             for _ in 1..workerCount do
+                 yield process' machine |]
+    
+    Source
+        .From(inputs)
+        .Via(distribute flows)
+        .RunFold([], (fun acc x -> x :: acc), materializer)
+
+renditions ["m1"; "m2"] (Array.init 20 string)
